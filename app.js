b9fccaf9e8329b562e0a2bb3fd311916b022f50c
herrberk4 <herrberk@gmail.com>
2022-11-01 17:40:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+/*const fruits= new Array();
+fruits[0] = 7;
+fruits[1] = 1;
+fruits[2] = 1 ;
+fruits[3] = 7;
+fruits[4] = 1 ;
+fruits[5] = 7;
+fruits[6] = 99;
+fruits[7] = 99;
+fruits[8] = 12 ;
+fruits[9] = 8;
+fruits[10] = 12;
+fruits[11] = 12 ;
+fruits[12] = 99;
+fruits[13] = 8;
+    for (let i=0;i<fruits.length;i++){  
+        let lives=0;      
+        for(let j=0;j<fruits.length;j++)
+        {
+            if(fruits[i]==fruits[j]){
+                lives=lives+1;
+            }               
+        } 
+        if(lives==2){
+            console.log(fruits[i])
+        }                      
+    }  */
+/*const fruits= new Array();
+fruits[0] = 0;
+fruits[1] = 1;
+fruits[2] = 0 ;
+fruits[3] = 1;
+fruits[4] = 0 ;
+fruits[5] = 99;
+fruits[6] = 1;
+fruits[7] = 99;
+    for (let i=0;i<fruits.length;i++){  
+        let lives=0;      
+        for(let j=0;j<fruits.length;j++)
+        {
+            if(fruits[i]==fruits[j]){
+                lives=lives+1;
+            }               
+        } 
+        if(lives==2){
+            console.log(fruits[i])
+        }                      
+    } 
+  */
+  /*const fruits= new Array();
+    fruits[0] = 2;   
+    fruits[1] = 2;    
+    fruits[2] = 3 ;   
+    fruits[3] = 2;    
+    fruits[4] = 3 ;    
+        for (let i=0;i<fruits.length;i++){  
+            let lives=0;      
+            for(let j=0;j<fruits.length;j++){
+                if(fruits[i]==fruits[j]){
+                    lives=lives+1;
+                }               
+            } 
+            if(lives==2){
+                console.log(fruits[i])
+            }                      
+        } 
+*/
+ 
+   
+    
+
